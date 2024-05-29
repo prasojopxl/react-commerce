729c328cd649292055ee539d92e5bf59f1be73e0
prasojopxl <prasojodesigner@gmail.com>
2024-05-29 21:20:48 +0700

    filter n sort

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 import ItemProduct from "@/components/ItemProduct";
 import { useQuery } from "@tanstack/react-query"
 import { getData } from "@/lib/services";
+import _ from "lodash";
+import { useRouter, useSearchParams } from "next/navigation"
 
 export default function ProductList() {
+    const searchParams = useSearchParams()
+    const paramsCategory = searchParams.get("category")
+    const paramsSort = searchParams.get("sort")
+
     const getQuery = async () => {
         return await getData("/products")
     }
@@ -35,10 +41,40 @@ export default function ProductList() {
     }
 
     const myData = query.data.data
+
+    const newData = paramsCategory && !paramsSort ?
+        _.filter(myData, (item) => {
+            return item.category === paramsCategory
+        }) :
+        !paramsCategory && paramsSort ?
+            _.filter(myData, (item) => {
+                return myData
+            }).sort((a, b) => {
+                if (paramsSort === "low") {
+                    return a.price - b.price
+                }
+                if (paramsSort === "high") {
+                    return b.price - a.price
+                }
+            })
+            :
+            paramsCategory && paramsSort ?
+                _.filter(myData, (item) => {
+                    return item.category === paramsCategory
+                }).sort((a, b) => {
+                    if (paramsSort === "low") {
+                        return a.price - b.price
+                    }
+                    if (paramsSort === "high") {
+                        return b.price - a.price
+                    }
+                })
+                : myData
+
     return (
         <div className="relative flex flex-wrap lg:-mx-4 -mx-2 ">
             {
-                myData.map(item => {
+                newData?.map(item => {
                     return (
                         <ItemProduct
                             key={item.id}
