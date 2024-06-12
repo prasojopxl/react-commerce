3af02b6b634a19028852aec87f9d546b85cd8d74
prasojopxl <prasojodesigner@gmail.com>
2024-06-12 22:01:26 +0700

    add dinamis data git status

--- app/api/payment/route.js
+++ app/api/payment/route.js
@@ -21,43 +21,47 @@ export function GET() {
 
 // create / post data
 export async function POST(req) {
-    // const data = await req.json()
+    const data = await req.json()
+    if (data.length === 0) {
+        throw new Error("data not found")
+    }
 
-    // if (data.length === 0) {
-    //     throw new Error("data not found")
-    // }
+    const itemDetails = data.data.map((item) => {
+        return {
+            key:item.id,
+            id: item.id,
+            name: item.name,
+            price: _.ceil(parseFloat(item.price.toString())),
+            quantity: item.quantity
+        }
+    })
 
-    // const itemDetails = data.map((item) => {
-    //     return {
-    //         key:item.id,
-    //         id: item.id,
-    //         name: item.name,
-    //         price: _.ceil(parseFloat(item.price.toString())),
-    //         quantity: item.quantity
-    //     }
-    // })
-
-    // const grossAmount = _.sumBy(itemDetails, (item)=> item.price * item.quantity)
-
+    const grossAmount = _.sumBy(itemDetails, (item)=> item.price * item.quantity)
     const parameter = {
-        item_details: [
-            {
-                id: _.random(100000, 999999),
-                price: 2000,
-                quantity: 1,
-                name:"ini name"
-            }
-        ],
+        transaction_details: [itemDetails],
         transaction_details: {
             "order_id":_.random(100000, 999999),
-            "gross_amount":2000
+            "gross_amount":grossAmount
         }
     }
 
+
+    // const parameter = {
+    //     item_details: [
+    //         {
+    //             id: _.random(100000, 999999),
+    //             price: 2000,
+    //             quantity: 1,
+    //             name:"ini name"
+    //         }
+    //     ],
+    //     transaction_details: {
+    //         "order_id":_.random(100000, 999999),
+    //         "gross_amount":2000
+    //     }
+    // }
+
     const token = await snap.createTransactionToken(parameter)
     
-    console.log(parameter)
-    return NextResponse.json({
-        token
-    })
+    return NextResponse.json({token})
 }
